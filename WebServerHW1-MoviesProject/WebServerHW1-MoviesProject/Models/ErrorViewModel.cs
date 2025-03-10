4158c0bb2ad81956071dc36a2588ffa6210e4fd3
ozan <123476414+ozan2604@users.noreply.github.com>
2025-03-10 21:17:39 +0300

    inital project

---
+++ WebServerHW1-MoviesProject/WebServerHW1-MoviesProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebServerHW1_MoviesProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
